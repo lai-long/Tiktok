2a4d41f9ddd032aa5df2ae2de355f8504e2ef4d5
lai-long <3496629516@qq.com>
2026-02-20 18:38:51 +0800

    feat 实现video list功能

--- biz/service/video.go
+++ biz/service/video.go
@@ -38,10 +38,27 @@ func VideoPublish(video dto.Video, data *multipart.FileHeader) (int, string) {
 	return consts.CodeSuccess, "success"
 }
 
-func VideoList(userId string, pageSize string, pageNum string) (int, string, []entity.VideoEntity) {
+func VideoList(userId string, pageSize string, pageNum string) (int, string, []dto.Video) {
 	videoList, err := db.GetVideoByUserID(userId, pageSize, pageNum)
 	if err != nil {
-		return consts.CodeDBSelectError, "VideoList GetVideoByUserID error", []entity.VideoEntity{}
+		return consts.CodeDBSelectError, "VideoList GetVideoByUserID error", []dto.Video{}
 	}
-	return consts.CodeSuccess, "success", videoList
+	videoDTOs := make([]dto.Video, len(videoList))
+	for i := 0; i < len(videoList); i++ {
+		videoDTOs[i] = dto.Video{
+			ID:           videoList[i].ID,
+			UserID:       videoList[i].UserID,
+			Title:        videoList[i].Title,
+			Description:  videoList[i].Description,
+			CommentCount: videoList[i].CommentCount,
+			CoverURL:     videoList[i].CoverURL,
+			CreatedAt:    videoList[i].CreatedAt,
+			DeletedAt:    videoList[i].DeletedAt,
+			LikeCount:    videoList[i].LikeCount,
+			UpdatedAt:    videoList[i].UpdatedAt,
+			VideoURL:     videoList[i].VideoURL,
+			VisitCount:   videoList[i].VisitCount,
+		}
+	}
+	return consts.CodeSuccess, "success", videoDTOs
 }
