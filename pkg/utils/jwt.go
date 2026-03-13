2306a3321ec9a032607141739e88219c269318f4
lai-long <3496629516@qq.com>
2026-03-13 20:25:06 +0800

    fix: 完善token生成

--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -11,24 +11,24 @@ import (
 func GenerateTokens(userDto dto.User) (string, string, bool) {
 	refreshTime := 288 * time.Hour
 	accessTime := 24 * time.Hour
-	refreshToken, err := GetToken(userDto.Username, userDto.ID, refreshTime)
+	refreshToken, err := GetToken(userDto.Username, userDto.ID, refreshTime, conf.Cfg.Jwt.RefreshSecret)
 	if err != nil {
 		return "生成refreshToken错误", "", false
 	}
-	accessToken, err := GetToken(userDto.Username, userDto.ID, accessTime)
+	accessToken, err := GetToken(userDto.Username, userDto.ID, accessTime, conf.Cfg.Jwt.AccessSecret)
 	if err != nil {
 		return "生成accessToken错误", "", false
 	}
 	return refreshToken, accessToken, true
 }
-func GetToken(username string, userid string, t time.Duration) (string, error) {
+func GetToken(username string, userid string, t time.Duration, secret string) (string, error) {
 	jwtClaims := &jwt.MapClaims{
 		"username": username,
 		"userid":   userid,
 		"exp":      time.Now().Add(t).Unix(),
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwtClaims)
-	tokenString, err := token.SignedString([]byte(conf.Cfg.Jwt.Secret))
+	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
 		return "生成token错误", err
 	}
