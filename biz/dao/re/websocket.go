591a440d9619a4124564f94b252a5b0d3601f63d
lai-long <3496629516@qq.com>
2026-03-25 12:49:16 +0800

    f

--- biz/dao/re/websocket.go
+++ biz/dao/re/websocket.go
@@ -6,21 +6,21 @@ import (
 	"time"
 )
 
-func SaveOfflineMsg(id, content string) {
+func (r *Redis) SaveOfflineMsg(id, content string) {
 	ctx := context.Background()
 	key := "offline:" + id
-	rdb.RPush(ctx, key, content)
-	rdb.Expire(ctx, key, 72*time.Hour)
+	r.redis.RPush(ctx, key, content)
+	r.redis.Expire(ctx, key, 72*time.Hour)
 }
-func FetchOfflineMsg(id string) ([]string, int) {
+func (r *Redis) FetchOfflineMsg(id string) ([]string, int) {
 	ctx := context.Background()
 	key := "offline:" + id
-	messages, err := rdb.LRange(ctx, key, 0, 100).Result()
+	messages, err := r.redis.LRange(ctx, key, 0, 100).Result()
 	if err != nil {
 		log.Println("redis FetchOfflineMsg err:", err)
 		return []string{}, 0
 	}
-	err = rdb.Del(ctx, key).Err()
+	err = r.redis.Del(ctx, key).Err()
 	if err != nil {
 		log.Println("redis FetchOfflineMsg del messages err:", err)
 		return []string{}, 0
