8c2435651e54ad1efb4433a637cdaa238ffedab4
lai-long <3496629516@qq.com>
2026-03-30 18:11:26 +0800

    fix:修改idl的package,完善following的数据库结构

--- biz/dao/social.go
+++ biz/dao/social.go
@@ -6,38 +6,28 @@ import (
 )
 
 func (m *MySQLdb) CreateFollowing(userId string, toUserId string) error {
-	sql := `INSERT INTO relations (user_id, following_id) VALUES (?,?)`
+	sql := `INSERT INTO following (user_id, following_id) VALUES (?,?)`
 	_, err := m.db.Exec(sql, userId, toUserId)
-	return err
-}
-func (m *MySQLdb) CreateFollower(userId string, toUserId string) error {
-	sql := `INSERT INTO relations (user_id, follower_id) VALUES (?,?)`
-	_, err := m.db.Exec(sql, toUserId, userId)
 	return err
 }
 func (m *MySQLdb) DeleteFollowing(userId string, toUserId string) error {
-	sql := `DELETE FROM relations WHERE user_id = ? AND follower_id = ?`
+	sql := `DELETE FROM following WHERE user_id = ? AND following_id = ?`
 	_, err := m.db.Exec(sql, userId, toUserId)
 	return err
 }
-func (m *MySQLdb) DeleteFollower(userId string, toUserId string) error {
-	sql := `DELETE FROM relations WHERE user_id = ? AND follower_id = ?`
-	_, err := m.db.Exec(sql, toUserId, userId)
-	return err
-}
-func (m *MySQLdb) FollowingIdList(userId string, pageNum int, pageSize int) ([]string, error) {
-	sql := `SELECT following_id FROM relations WHERE user_id = ? AND following_id IS NOT NULL LIMIT ? OFFSET ?`
+func (m *MySQLdb) FollowingList(userId string, pageNum int, pageSize int) ([]entity.UserEntity, error) {
+	sql := `SELECT * FROM users WHERE id IN (SELECT following_id FROM following WHERE user_id = ?) LIMIT ? OFFSET ?`
+	var users []entity.UserEntity
 	offset := pageNum * pageSize
-	var followingIds []string
-	err := m.db.Select(&followingIds, sql, userId, pageSize, offset)
-	return followingIds, err
+	err := m.db.Select(&users, sql, userId, pageSize, offset)
+	return users, err
 }
-func (m *MySQLdb) FollowerIdList(userId string, pageNum int, pageSize int) ([]string, error) {
-	sql := `SELECT follower_id FROM relations WHERE user_id = ? AND follower_id IS NOT NULL LIMIT ? OFFSET ?`
+func (m *MySQLdb) FollowerList(userId string, pageNum int, pageSize int) ([]entity.UserEntity, error) {
+	sql := `SELECT * FROM users WHERE id IN (SELECT user_id FROM following WHERE following_id = ?) LIMIT ? OFFSET ?`
 	offset := pageNum * pageSize
-	var followerIds []string
-	err := m.db.Select(&followerIds, sql, userId, pageSize, offset)
-	return followerIds, err
+	var users []entity.UserEntity
+	err := m.db.Select(&users, sql, userId, pageSize, offset)
+	return users, err
 }
 func (m *MySQLdb) FriendList(userId string, pageNum int, pageSize int) ([]entity.UserEntity, bool) {
 	offset := pageNum * pageSize
