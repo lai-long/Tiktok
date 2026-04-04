147ceee6df42ff9c2d1b5a08ed9128f1e54acd41
lai-long <3496629516@qq.com>
2026-04-04 12:42:19 +0800

    fix:使用refresh resp和req

--- biz/model/user/user.pb.go
+++ biz/model/user/user.pb.go
@@ -540,6 +540,116 @@ func (x *UserAvatarResp) GetData() *UserInfo {
 	return nil
 }
 
+type RefreshTokenReq struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	RefreshToken string `protobuf:"bytes,1,opt,name=RefreshToken,proto3" form:"refresh_token" json:"refresh_token,omitempty"`
+}
+
+func (x *RefreshTokenReq) Reset() {
+	*x = RefreshTokenReq{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_user_proto_msgTypes[9]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *RefreshTokenReq) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*RefreshTokenReq) ProtoMessage() {}
+
+func (x *RefreshTokenReq) ProtoReflect() protoreflect.Message {
+	mi := &file_user_proto_msgTypes[9]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use RefreshTokenReq.ProtoReflect.Descriptor instead.
+func (*RefreshTokenReq) Descriptor() ([]byte, []int) {
+	return file_user_proto_rawDescGZIP(), []int{9}
+}
+
+func (x *RefreshTokenReq) GetRefreshToken() string {
+	if x != nil {
+		return x.RefreshToken
+	}
+	return ""
+}
+
+type RefreshTokenResp struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Base         *common.Base `protobuf:"bytes,1,opt,name=Base,proto3" form:"Base" json:"base" query:"Base"`
+	RefreshToken string       `protobuf:"bytes,2,opt,name=RefreshToken,proto3" form:"RefreshToken" json:"refresh_token" query:"RefreshToken"`
+	AccessToken  string       `protobuf:"bytes,3,opt,name=AccessToken,proto3" form:"AccessToken" json:"access_token" query:"AccessToken"`
+}
+
+func (x *RefreshTokenResp) Reset() {
+	*x = RefreshTokenResp{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_user_proto_msgTypes[10]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *RefreshTokenResp) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*RefreshTokenResp) ProtoMessage() {}
+
+func (x *RefreshTokenResp) ProtoReflect() protoreflect.Message {
+	mi := &file_user_proto_msgTypes[10]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use RefreshTokenResp.ProtoReflect.Descriptor instead.
+func (*RefreshTokenResp) Descriptor() ([]byte, []int) {
+	return file_user_proto_rawDescGZIP(), []int{10}
+}
+
+func (x *RefreshTokenResp) GetBase() *common.Base {
+	if x != nil {
+		return x.Base
+	}
+	return nil
+}
+
+func (x *RefreshTokenResp) GetRefreshToken() string {
+	if x != nil {
+		return x.RefreshToken
+	}
+	return ""
+}
+
+func (x *RefreshTokenResp) GetAccessToken() string {
+	if x != nil {
+		return x.AccessToken
+	}
+	return ""
+}
+
 var File_user_proto protoreflect.FileDescriptor
 
 var file_user_proto_rawDesc = []byte{
@@ -625,9 +735,26 @@ var file_user_proto_rawDesc = []byte{
 	0x73, 0x65, 0x12, 0x33, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b,
 	0x32, 0x0e, 0x2e, 0x75, 0x73, 0x65, 0x72, 0x2e, 0x55, 0x73, 0x65, 0x72, 0x49, 0x6e, 0x66, 0x6f,
 	0x42, 0x0f, 0xca, 0xf3, 0x18, 0x0b, 0x6a, 0x73, 0x6f, 0x6e, 0x3a, 0x22, 0x64, 0x61, 0x74, 0x61,
-	0x22, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x42, 0x17, 0x5a, 0x15, 0x54, 0x69, 0x6b, 0x74, 0x6f,
-	0x6b, 0x2f, 0x62, 0x69, 0x7a, 0x2f, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x2f, 0x75, 0x73, 0x65, 0x72,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x22, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x22, 0x48, 0x0a, 0x0f, 0x52, 0x65, 0x66, 0x72, 0x65,
+	0x73, 0x68, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x52, 0x65, 0x71, 0x12, 0x35, 0x0a, 0x0c, 0x52, 0x65,
+	0x66, 0x72, 0x65, 0x73, 0x68, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
+	0x42, 0x11, 0xca, 0xbb, 0x18, 0x0d, 0x72, 0x65, 0x66, 0x72, 0x65, 0x73, 0x68, 0x5f, 0x74, 0x6f,
+	0x6b, 0x65, 0x6e, 0x52, 0x0c, 0x52, 0x65, 0x66, 0x72, 0x65, 0x73, 0x68, 0x54, 0x6f, 0x6b, 0x65,
+	0x6e, 0x22, 0xbe, 0x01, 0x0a, 0x10, 0x52, 0x65, 0x66, 0x72, 0x65, 0x73, 0x68, 0x54, 0x6f, 0x6b,
+	0x65, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x12, 0x31, 0x0a, 0x04, 0x42, 0x61, 0x73, 0x65, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x0b, 0x32, 0x0c, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e, 0x42, 0x61,
+	0x73, 0x65, 0x42, 0x0f, 0xca, 0xf3, 0x18, 0x0b, 0x6a, 0x73, 0x6f, 0x6e, 0x3a, 0x22, 0x62, 0x61,
+	0x73, 0x65, 0x22, 0x52, 0x04, 0x42, 0x61, 0x73, 0x65, 0x12, 0x3c, 0x0a, 0x0c, 0x52, 0x65, 0x66,
+	0x72, 0x65, 0x73, 0x68, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x42,
+	0x18, 0xca, 0xf3, 0x18, 0x14, 0x6a, 0x73, 0x6f, 0x6e, 0x3a, 0x22, 0x72, 0x65, 0x66, 0x72, 0x65,
+	0x73, 0x68, 0x5f, 0x74, 0x6f, 0x6b, 0x65, 0x6e, 0x22, 0x52, 0x0c, 0x52, 0x65, 0x66, 0x72, 0x65,
+	0x73, 0x68, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x12, 0x39, 0x0a, 0x0b, 0x41, 0x63, 0x63, 0x65, 0x73,
+	0x73, 0x54, 0x6f, 0x6b, 0x65, 0x6e, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x42, 0x17, 0xca, 0xf3,
+	0x18, 0x13, 0x6a, 0x73, 0x6f, 0x6e, 0x3a, 0x22, 0x61, 0x63, 0x63, 0x65, 0x73, 0x73, 0x5f, 0x74,
+	0x6f, 0x6b, 0x65, 0x6e, 0x22, 0x52, 0x0b, 0x41, 0x63, 0x63, 0x65, 0x73, 0x73, 0x54, 0x6f, 0x6b,
+	0x65, 0x6e, 0x42, 0x17, 0x5a, 0x15, 0x54, 0x69, 0x6b, 0x74, 0x6f, 0x6b, 0x2f, 0x62, 0x69, 0x7a,
+	0x2f, 0x6d, 0x6f, 0x64, 0x65, 0x6c, 0x2f, 0x75, 0x73, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
@@ -642,32 +769,35 @@ func file_user_proto_rawDescGZIP() []byte {
 	return file_user_proto_rawDescData
 }
 
-var file_user_proto_msgTypes = make([]protoimpl.MessageInfo, 9)
+var file_user_proto_msgTypes = make([]protoimpl.MessageInfo, 11)
 var file_user_proto_goTypes = []interface{}{
-	(*UserInfo)(nil),       // 0: user.UserInfo
-	(*RegisterReq)(nil),    // 1: user.RegisterReq
-	(*RegisterResp)(nil),   // 2: user.RegisterResp
-	(*LoginReq)(nil),       // 3: user.LoginReq
-	(*LoginResp)(nil),      // 4: user.LoginResp
-	(*UserInfoReq)(nil),    // 5: user.UserInfoReq
-	(*UserInfoResp)(nil),   // 6: user.UserInfoResp
-	(*UserAvatarReq)(nil),  // 7: user.UserAvatarReq
-	(*UserAvatarResp)(nil), // 8: user.UserAvatarResp
-	(*common.Base)(nil),    // 9: common.Base
+	(*UserInfo)(nil),         // 0: user.UserInfo
+	(*RegisterReq)(nil),      // 1: user.RegisterReq
+	(*RegisterResp)(nil),     // 2: user.RegisterResp
+	(*LoginReq)(nil),         // 3: user.LoginReq
+	(*LoginResp)(nil),        // 4: user.LoginResp
+	(*UserInfoReq)(nil),      // 5: user.UserInfoReq
+	(*UserInfoResp)(nil),     // 6: user.UserInfoResp
+	(*UserAvatarReq)(nil),    // 7: user.UserAvatarReq
+	(*UserAvatarResp)(nil),   // 8: user.UserAvatarResp
+	(*RefreshTokenReq)(nil),  // 9: user.RefreshTokenReq
+	(*RefreshTokenResp)(nil), // 10: user.RefreshTokenResp
+	(*common.Base)(nil),      // 11: common.Base
 }
 var file_user_proto_depIdxs = []int32{
-	9, // 0: user.RegisterResp.Base:type_name -> common.Base
-	9, // 1: user.LoginResp.Base:type_name -> common.Base
-	0, // 2: user.LoginResp.data:type_name -> user.UserInfo
-	9, // 3: user.UserInfoResp.Base:type_name -> common.Base
-	0, // 4: user.UserInfoResp.Data:type_name -> user.UserInfo
-	9, // 5: user.UserAvatarResp.Base:type_name -> common.Base
-	0, // 6: user.UserAvatarResp.data:type_name -> user.UserInfo
-	7, // [7:7] is the sub-list for method output_type
-	7, // [7:7] is the sub-list for method input_type
-	7, // [7:7] is the sub-list for extension type_name
-	7, // [7:7] is the sub-list for extension extendee
-	0, // [0:7] is the sub-list for field type_name
+	11, // 0: user.RegisterResp.Base:type_name -> common.Base
+	11, // 1: user.LoginResp.Base:type_name -> common.Base
+	0,  // 2: user.LoginResp.data:type_name -> user.UserInfo
+	11, // 3: user.UserInfoResp.Base:type_name -> common.Base
+	0,  // 4: user.UserInfoResp.Data:type_name -> user.UserInfo
+	11, // 5: user.UserAvatarResp.Base:type_name -> common.Base
+	0,  // 6: user.UserAvatarResp.data:type_name -> user.UserInfo
+	11, // 7: user.RefreshTokenResp.Base:type_name -> common.Base
+	8,  // [8:8] is the sub-list for method output_type
+	8,  // [8:8] is the sub-list for method input_type
+	8,  // [8:8] is the sub-list for extension type_name
+	8,  // [8:8] is the sub-list for extension extendee
+	0,  // [0:8] is the sub-list for field type_name
 }
 
 func init() { file_user_proto_init() }
@@ -784,6 +914,30 @@ func file_user_proto_init() {
 				return nil
 			}
 		}
+		file_user_proto_msgTypes[9].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*RefreshTokenReq); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_user_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*RefreshTokenResp); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -791,7 +945,7 @@ func file_user_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_user_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   9,
+			NumMessages:   11,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
