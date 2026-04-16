17afea11bcdc8b1f149c57a9470fac011615caa1
lai-long <3496629516@qq.com>
2026-04-16 22:00:33 +0800

    fix:将部分删除改为软删除

--- biz/dal/dao/social.go
+++ biz/dal/dao/social.go
@@ -11,19 +11,19 @@ func (m *MySQLdb) CreateFollowing(userId string, toUserId string) error {
 	return err
 }
 func (m *MySQLdb) DeleteFollowing(userId string, toUserId string) error {
-	sql := `DELETE FROM following WHERE user_id = ? AND following_id = ?`
+	sql := `UPDATE following SET deleted_at = NOW() WHERE user_id = ? AND following_id = ? AND deleted_at IS NULL`
 	_, err := m.db.Exec(sql, userId, toUserId)
 	return err
 }
 func (m *MySQLdb) FollowingList(userId string, pageNum int64, pageSize int64) ([]entity.UserEntity, error) {
-	sql := `SELECT * FROM users WHERE id IN (SELECT following_id FROM following WHERE user_id = ?) LIMIT ? OFFSET ?`
+	sql := `SELECT * FROM users WHERE id IN (SELECT following_id FROM following WHERE user_id = ? AND deleted_at is null) LIMIT ? OFFSET ?`
 	var users []entity.UserEntity
 	offset := pageNum * pageSize
 	err := m.db.Select(&users, sql, userId, pageSize, offset)
 	return users, err
 }
 func (m *MySQLdb) FollowerList(userId string, pageNum int64, pageSize int64) ([]entity.UserEntity, error) {
-	sql := `SELECT * FROM users WHERE id IN (SELECT user_id FROM following WHERE following_id = ?) LIMIT ? OFFSET ?`
+	sql := `SELECT * FROM users WHERE id IN (SELECT user_id FROM following WHERE following_id = ? and users.deleted_at is null) LIMIT ? OFFSET ?`
 	offset := pageNum * pageSize
 	var users []entity.UserEntity
 	err := m.db.Select(&users, sql, userId, pageSize, offset)
@@ -31,8 +31,8 @@ func (m *MySQLdb) FollowerList(userId string, pageNum int64, pageSize int64) ([]
 }
 func (m *MySQLdb) FriendList(userId string, pageNum int64, pageSize int64) ([]entity.UserEntity, bool) {
 	offset := pageNum * pageSize
-	sql := `SELECT * FROM users WHERE id IN(SELECT following_id FROM following WHERE user_id = ?) 
-                      AND id IN (SELECT user_id from following where following_id=?) LIMIT ? OFFSET ?`
+	sql := `SELECT * FROM users WHERE id IN(SELECT following_id FROM following WHERE user_id = ? and deleted_at is null)
+                      AND id IN (SELECT user_id from following where following_id=? and deleted_at is null) LIMIT ? OFFSET ?`
 	var users []entity.UserEntity
 	err := m.db.Select(&users, sql, userId, userId, pageSize, offset)
 	if err != nil {
