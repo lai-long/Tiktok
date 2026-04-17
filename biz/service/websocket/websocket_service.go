4a1663b7f1224e535515eb0c70af2674be95cf6c
lai-long <3496629516@qq.com>
2026-04-17 10:40:37 +0800

    fix:将websocket服务拆分成多个子函数

--- biz/service/websocket/websocket_service.go
+++ biz/service/websocket/websocket_service.go
@@ -172,161 +172,184 @@ func (ws *WebsocketService) Start() {
 	for {
 		select {
 		case client := <-ws.Manager.Register:
-			log.Println("建立websocket连接", client.ID)
-			if client.GroupId != "" {
-				ws.Manager.mu.Lock()
-				ws.Manager.Groups[client.GroupId] = append(ws.Manager.Groups[client.GroupId], client)
-				ws.Manager.mu.Unlock()
-			}
-			ws.Manager.Clients[client.ID] = client
-			replyMSg := chat.ReplyMsg{
-				From:    client.ID,
-				Code:    consts.Success,
-				Content: "连接成功",
-			}
-			msg, _ := protojson.Marshal(&replyMSg)
-			client.Send <- msg
+			ws.startRegister(client)
 		case client := <-ws.Manager.Unregister:
-			log.Println("断开websocket连接", client.ID)
-			if client.GroupId != "" {
-				ws.Manager.mu.Lock()
-				for i, v := range ws.Manager.Groups[client.GroupId] {
-					if v.ID == client.ID {
-						ws.Manager.Groups[client.GroupId] = append(ws.Manager.Groups[client.GroupId][:i], ws.Manager.Groups[client.GroupId][i+1:]...)
-						break
-					}
-				}
-				ws.Manager.mu.Unlock()
-			}
-			if _, ok := ws.Manager.Clients[client.ID]; ok {
-				replyMSg := chat.ReplyMsg{
-					From:    client.ID,
-					Code:    consts.Success,
-					Content: "连接中断",
-				}
-				msg, _ := protojson.Marshal(&replyMSg)
-				client.Send <- msg
-				close(client.Send)
-				delete(ws.Manager.Clients, client.ID)
-			}
+			ws.startUnregister(client)
 		case broadcast := <-ws.Manager.Broadcast:
 			if broadcast.Type == "1" {
-				message := broadcast.Message
-				sendId := broadcast.Clients.SendID
-				flag := false
-				ws.Manager.mu.Lock()
-				for id, client := range ws.Manager.Clients {
-					if id != sendId {
-						continue
-					}
-					replyMSg := chat.ReplyMsg{
-						From:    client.ID,
-						Code:    consts.Success,
-						Content: string(message),
-					}
-					msg, _ := protojson.Marshal(&replyMSg)
-					select {
-					case client.Send <- msg:
-						flag = true
-					default:
-						close(client.Send)
-						delete(ws.Manager.Clients, client.ID)
-					}
-				}
-				ws.Manager.mu.Unlock()
-				id := broadcast.Clients.ID
-				if flag {
-					replyMSg := chat.ReplyMsg{
-						From:    broadcast.Clients.ID,
-						Code:    consts.Success,
-						Content: "对方在线",
-					}
-					msg, _ := protojson.Marshal(&replyMSg)
-					broadcast.Clients.Send <- msg
-					err := ws.mysql.InsertMsg(id, string(message))
-					if err != nil {
-						log.Println("Insert message error:", err)
-					}
-				} else {
-					replyMSg := chat.ReplyMsg{
-						From:    broadcast.Clients.ID,
-						Code:    consts.Success,
-						Content: "对方不在线",
-					}
-					err := ws.mysql.InsertMsg(id, string(message))
-					if err != nil {
-						log.Println("Insert message error:", err)
-					}
-					err = ws.redis.SaveOfflineMsg(broadcast.Clients.SendID, string(message))
-					if err != nil {
-						log.Println("Save offline message error:", err)
-					}
-					msg, _ := protojson.Marshal(&replyMSg)
-					broadcast.Clients.Send <- msg
-				}
+				ws.startBroadcastOneOnline(broadcast)
 			} else if broadcast.Type == "2" {
-				message, err := ws.redis.FetchOfflineMsg(broadcast.Clients.SendID)
-				if err != nil {
-					log.Println("Fetch offline message error:", err)
-					replyMSg := chat.ReplyMsg{
-						From:    "系统",
-						Code:    consts.Success,
-						Content: "获取离线消息失败",
-					}
-					msg, _ := protojson.Marshal(&replyMSg)
-					broadcast.Clients.Send <- msg
-					continue
-				}
-				str := strings.Join(message, ",\n ")
-				finalInfo := str + fmt.Sprintf("\ntotal:%d", len(message))
-				replyMSg := chat.ReplyMsg{
-					From:    "未在线时收到消息",
-					Code:    consts.Success,
-					Content: finalInfo,
-				}
-				msg, _ := protojson.Marshal(&replyMSg)
-				broadcast.Clients.Send <- msg
+				ws.startBroadcastOneOffline(broadcast)
 			} else if broadcast.Type == "3" {
-				pageNum := 0
-				pageSize := 10
-				if broadcast.PageNum != "" {
-					pageNum, _ = strconv.Atoi(broadcast.PageNum)
-				}
-				if broadcast.PageSize != "" {
-					pageSize, _ = strconv.Atoi(broadcast.PageSize)
-				}
-
-				msgs, err := ws.mysql.GetWebsocketHistory(broadcast.Clients.ID, broadcast.Clients.SendID, pageNum, pageSize)
-				if err != nil || msgs == nil {
-					replyMSg := chat.ReplyMsg{
-						From:    "系统",
-						Code:    consts.Success,
-						Content: "获取历史消息失败",
-					}
-					msg, _ := protojson.Marshal(&replyMSg)
-					broadcast.Clients.Send <- msg
-					continue
-				}
-				str := strings.Join(msgs, ",\n ")
-				finalInfo := str + fmt.Sprintf("\ntotal:%d", len(msgs))
-				replyMSg := chat.ReplyMsg{
-					From:    broadcast.Clients.ID + "and" + broadcast.Clients.SendID,
-					Code:    consts.Success,
-					Content: finalInfo,
-				}
-				msg, _ := protojson.Marshal(&replyMSg)
-				broadcast.Clients.Send <- msg
+				ws.startBroadcastOneHistory(broadcast)
 			}
 		case groupBroadcast := <-ws.Manager.GroupBroadcast:
-			for _, client := range groupBroadcast.Clients {
-				replyMSg := chat.ReplyMsg{
-					From:    client.ID,
-					Code:    consts.Success,
-					Content: string(groupBroadcast.Message),
-				}
-				msg, _ := protojson.Marshal(&replyMSg)
-				client.Send <- msg
+			ws.startBroadcastGroupOnline(groupBroadcast)
+		}
+	}
+}
+
+func (ws *WebsocketService) startRegister(client *Client) {
+	log.Println("建立websocket连接", client.ID)
+	if client.GroupId != "" {
+		ws.Manager.mu.Lock()
+		ws.Manager.Groups[client.GroupId] = append(ws.Manager.Groups[client.GroupId], client)
+		ws.Manager.mu.Unlock()
+	}
+	ws.Manager.Clients[client.ID] = client
+	replyMSg := chat.ReplyMsg{
+		From:    client.ID,
+		Code:    consts.Success,
+		Content: "连接成功",
+	}
+	msg, _ := protojson.Marshal(&replyMSg)
+	client.Send <- msg
+}
+
+func (ws *WebsocketService) startUnregister(client *Client) {
+	log.Println("断开websocket连接", client.ID)
+	if client.GroupId != "" {
+		ws.Manager.mu.Lock()
+		for i, v := range ws.Manager.Groups[client.GroupId] {
+			if v.ID == client.ID {
+				ws.Manager.Groups[client.GroupId] = append(ws.Manager.Groups[client.GroupId][:i], ws.Manager.Groups[client.GroupId][i+1:]...)
+				break
 			}
 		}
+		ws.Manager.mu.Unlock()
+	}
+	if _, ok := ws.Manager.Clients[client.ID]; ok {
+		replyMSg := chat.ReplyMsg{
+			From:    client.ID,
+			Code:    consts.Success,
+			Content: "连接中断",
+		}
+		msg, _ := protojson.Marshal(&replyMSg)
+		client.Send <- msg
+		close(client.Send)
+		delete(ws.Manager.Clients, client.ID)
+	}
+}
+
+func (ws *WebsocketService) startBroadcastOneOnline(broadcast *Broadcast) {
+	message := broadcast.Message
+	sendId := broadcast.Clients.SendID
+	flag := false
+	ws.Manager.mu.Lock()
+	for id, client := range ws.Manager.Clients {
+		if id != sendId {
+			continue
+		}
+		replyMSg := chat.ReplyMsg{
+			From:    client.ID,
+			Code:    consts.Success,
+			Content: string(message),
+		}
+		msg, _ := protojson.Marshal(&replyMSg)
+		select {
+		case client.Send <- msg:
+			flag = true
+		default:
+			close(client.Send)
+			delete(ws.Manager.Clients, client.ID)
+		}
+	}
+	ws.Manager.mu.Unlock()
+	id := broadcast.Clients.ID
+	if flag {
+		replyMSg := chat.ReplyMsg{
+			From:    broadcast.Clients.ID,
+			Code:    consts.Success,
+			Content: "对方在线",
+		}
+		msg, _ := protojson.Marshal(&replyMSg)
+		broadcast.Clients.Send <- msg
+		err := ws.mysql.InsertMsg(id, string(message))
+		if err != nil {
+			log.Println("Insert message error:", err)
+		}
+	} else {
+		replyMSg := chat.ReplyMsg{
+			From:    broadcast.Clients.ID,
+			Code:    consts.Success,
+			Content: "对方不在线",
+		}
+		err := ws.mysql.InsertMsg(id, string(message))
+		if err != nil {
+			log.Println("Insert message error:", err)
+		}
+		err = ws.redis.SaveOfflineMsg(broadcast.Clients.SendID, string(message))
+		if err != nil {
+			log.Println("Save offline message error:", err)
+		}
+		msg, _ := protojson.Marshal(&replyMSg)
+		broadcast.Clients.Send <- msg
+	}
+}
+
+func (ws *WebsocketService) startBroadcastOneOffline(broadcast *Broadcast) {
+	message, err := ws.redis.FetchOfflineMsg(broadcast.Clients.SendID)
+	if err != nil {
+		log.Println("Fetch offline message error:", err)
+		replyMSg := chat.ReplyMsg{
+			From:    "系统",
+			Code:    consts.Success,
+			Content: "获取离线消息失败",
+		}
+		msg, _ := protojson.Marshal(&replyMSg)
+		broadcast.Clients.Send <- msg
+		return
+	}
+	str := strings.Join(message, ",\n ")
+	finalInfo := str + fmt.Sprintf("\ntotal:%d", len(message))
+	replyMSg := chat.ReplyMsg{
+		From:    "未在线时收到消息",
+		Code:    consts.Success,
+		Content: finalInfo,
+	}
+	msg, _ := protojson.Marshal(&replyMSg)
+	broadcast.Clients.Send <- msg
+}
+
+func (ws *WebsocketService) startBroadcastOneHistory(broadcast *Broadcast) {
+	pageNum := 0
+	pageSize := 10
+	if broadcast.PageNum != "" {
+		pageNum, _ = strconv.Atoi(broadcast.PageNum)
+	}
+	if broadcast.PageSize != "" {
+		pageSize, _ = strconv.Atoi(broadcast.PageSize)
+	}
+	msgs, err := ws.mysql.GetWebsocketHistory(broadcast.Clients.ID, broadcast.Clients.SendID, pageNum, pageSize)
+	if err != nil || msgs == nil {
+		replyMSg := chat.ReplyMsg{
+			From:    "系统",
+			Code:    consts.Success,
+			Content: "获取历史消息失败",
+		}
+		msg, _ := protojson.Marshal(&replyMSg)
+		broadcast.Clients.Send <- msg
+		return
+	}
+	str := strings.Join(msgs, ",\n ")
+	finalInfo := str + fmt.Sprintf("\ntotal:%d", len(msgs))
+	replyMSg := chat.ReplyMsg{
+		From:    broadcast.Clients.ID + "and" + broadcast.Clients.SendID,
+		Code:    consts.Success,
+		Content: finalInfo,
+	}
+	msg, _ := protojson.Marshal(&replyMSg)
+	broadcast.Clients.Send <- msg
+}
+
+func (ws *WebsocketService) startBroadcastGroupOnline(groupBroadcast *GroupBroadcast) {
+	for _, client := range groupBroadcast.Clients {
+		replyMSg := chat.ReplyMsg{
+			From:    client.ID,
+			Code:    consts.Success,
+			Content: string(groupBroadcast.Message),
+		}
+		msg, _ := protojson.Marshal(&replyMSg)
+		client.Send <- msg
 	}
 }
