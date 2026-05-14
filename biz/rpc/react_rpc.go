8934e1ca2bea5bef5aaf838157e4b4451fddebae
lai-long <3496629516@qq.com>
2026-05-14 13:05:26 +0800

    fix:对react模块实现服务注册与发现

---
+++ biz/rpc/react_rpc.go
@@ -0,0 +1,112 @@
+package rpc
+
+import (
+	"Tiktok/kitex_gen/react"
+	"Tiktok/kitex_gen/react/commentservice"
+	"Tiktok/kitex_gen/react/likeservice"
+	"Tiktok/pkg/consts"
+	"context"
+	"log"
+
+	model "Tiktok/biz/model/react"
+	video "Tiktok/biz/model/video"
+
+	"github.com/cloudwego/kitex/client"
+	etcd "github.com/kitex-contrib/registry-etcd"
+)
+
+func InitReactRpc() {
+	r, err := etcd.NewEtcdResolver([]string{"127.0.0.1:2379"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	commentCli, err := commentservice.NewClient("commentService", client.WithResolver(r))
+	if err != nil {
+		log.Fatal(err)
+	}
+	likeCli, err := likeservice.NewClient("likeService", client.WithResolver(r))
+	if err != nil {
+		log.Fatal(err)
+	}
+	commentClient = commentCli
+	likeClient = likeCli
+}
+
+func CommentPublish(ctx context.Context, req *react.CommentPublishReq) (int32, error) {
+	resp, err := commentClient.CommentPublish(ctx, req)
+	if err != nil || resp == nil {
+		return consts.ReactError, err
+	}
+	return resp.Code, nil
+}
+
+func CommentList(ctx context.Context, req *react.CommentListReq) (int32, *model.CommentData, error) {
+	resp, err := commentClient.CommentList(ctx, req)
+	if err != nil || resp.Data == nil {
+		return consts.ReactDBSelectError, nil, err
+	}
+	infos := make([]*model.CommentInfo, 0, resp.Data.Total)
+	for i := 0; i < len(resp.Data.Items); i++ {
+		infos = append(infos, &model.CommentInfo{
+			UserId:       resp.Data.Items[i].UserId,
+			TargetId:     resp.Data.Items[i].TargetId,
+			CommentId:    resp.Data.Items[i].CommentId,
+			Content:      resp.Data.Items[i].Content,
+			LikeCount:    resp.Data.Items[i].LikeCount,
+			CreatedAt:    resp.Data.Items[i].CreatedAt,
+			UpdatedAt:    resp.Data.Items[i].UpdatedAt,
+			DeletedAt:    resp.Data.Items[i].DeletedAt,
+			TargetType:   resp.Data.Items[i].TargetType,
+			CommentCount: resp.Data.Items[i].CommentCount,
+		})
+	}
+	data := &model.CommentData{
+		Items: infos,
+		Total: int64(len(infos)),
+	}
+	return resp.Code, data, err
+}
+
+func CommentDelete(ctx context.Context, req *react.CommentDeleteReq) (int32, error) {
+	resp, err := commentClient.CommentDelete(ctx, req)
+	if err != nil || resp == nil {
+		return consts.ReactError, err
+	}
+	return resp.Code, nil
+}
+
+func LikeAction(ctx context.Context, req *react.LikeActionReq) (int32, error) {
+	resp, err := likeClient.LikeAction(ctx, req)
+	if err != nil || resp == nil {
+		return consts.ReactError, err
+	}
+	return resp.Code, nil
+}
+
+func LikeList(ctx context.Context, req *react.LikeListReq) (int32, *model.LikeVideoData, error) {
+	resp, err := likeClient.LikeList(ctx, req)
+	if err != nil || resp.Data == nil {
+		return consts.ReactDBSelectError, nil, err
+	}
+	infos := make([]*video.VideoInfo, 0, resp.Data.Total)
+	for i := 0; i < len(resp.Data.Items); i++ {
+		infos = append(infos, &video.VideoInfo{
+			ID:           resp.Data.Items[i].ID,
+			UserID:       resp.Data.Items[i].UserID,
+			Title:        resp.Data.Items[i].Title,
+			Description:  resp.Data.Items[i].Description,
+			CommentCount: resp.Data.Items[i].CommentCount,
+			CoverURL:     resp.Data.Items[i].CoverURL,
+			CreatedAt:    resp.Data.Items[i].CreatedAt,
+			LikeCount:    resp.Data.Items[i].LikeCount,
+			UpdatedAt:    resp.Data.Items[i].UpdatedAt,
+			VideoURL:     resp.Data.Items[i].VideoURL,
+			VisitCount:   resp.Data.Items[i].VisitCount,
+		})
+	}
+	data := &model.LikeVideoData{
+		Items: infos,
+		Total: int64(len(infos)),
+	}
+	return resp.Code, data, err
+}
